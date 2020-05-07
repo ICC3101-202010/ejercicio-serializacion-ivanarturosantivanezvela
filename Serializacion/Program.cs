f4593979418c76a1fc470020f3e0df30f2cf9980
Ivan Arturo Santivañez Vela <Ivan@MacBook-Pro-de-Clementina-757.local>
2020-05-07 08:55:31 -0400

    gitignore

---
+++ Serializacion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Serializacion
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
